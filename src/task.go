3a5635525138ad299e1c17c13b6ec529a00030bc
zecraid <zecraid@gmail.com>
2024-02-25 13:52:03 +0800

    add Dockerfile

--- src/task.go
+++ src/task.go
@@ -2,40 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
 	"time"
 )
 
-const URL = "deviceshifu-plate-reader.deviceshifu.svc.cluster.local/get_measurement"
 const TimeInterval = 10
 
 func main() {
 	for {
-		_, err := getData()
-		if err != nil {
-			fmt.Printf("Error getting measurement: %v\n", err)
-			continue
-		}
-
+		fmt.Println("Test Shifu")
 		time.Sleep(time.Second * time.Duration(TimeInterval))
 	}
-}
-
-func getData() ([]float64, error) {
-	resp, err := http.Get(URL)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	valuesStr := strings.Fields(string(body))
-	fmt.Println(valuesStr)
-	return nil, nil
 }
